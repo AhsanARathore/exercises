e10ce3201c39f2c03db85639ce624ce5c625841d
Rathore <rathore@Rathores-MBP.attlocal.net>
2022-06-22 12:35:37 -0500

    adding solution to loan Calculator

--- jasmine-testing-exercises/calculator/calculator.js
+++ jasmine-testing-exercises/calculator/calculator.js
@@ -21,20 +21,38 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const values = {amount: 50000, years: 10, rate: 3};
+  const amounthtml = document.getElementById("loan-amount");
+  amounthtml.value = values.amount;
+  const yearshtml = document.getElementById("loan-years");
+  yearshtml.value = values.years;
+  const ratehtml = document.getElementById("loan-rate");
+  ratehtml.value = values.rate;
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const currenthtmlValues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currenthtmlValues));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const monthlyRate = (values.rate / 100) / 12;
+  const n = Math.floor(values.years * 12);
+  return (
+    (monthlyRate * values.amount) /
+    (1 - Math.pow((1 + monthlyRate), -n))
+  ).toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyhtml = document.getElementById("monthly-payment");
+  monthlyhtml.innerText = "$" + monthly;
 }
